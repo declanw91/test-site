9fe0dfc48b8f4e0c2af4ee228b144fb2200c50eb
declanw91 <dwright1991@gmail.com>
2017-08-20 18:12:45 +0100

    google maps page
    Added reset and zoombuttons and updated CSS

--- gmaps/js/mapApp.js
+++ gmaps/js/mapApp.js
@@ -1,14 +1,14 @@
 var map;
+var mapPoints = [{"Name":"London", "Lat": 51.5, "Lng":-0.11},{"Name": "StatueOfLiberty", "Lat": 40.6891, "Lng":-74.0445}];
+var defaultMapOptions = {center: new google.maps.LatLng(mapPoints[0].Lat,mapPoints[0].Lng), zoom: 8};
 function initMap() {
 	var infowindow = new google.maps.InfoWindow();
-  var london = new google.maps.LatLng(51.5,-0.11);
+  var london = new google.maps.LatLng(mapPoints[0].Lat,mapPoints[0].Lng);
   var mapDiv = document.getElementById('map');
-  map = new google.maps.Map(mapDiv, {
-    center: london,
-    zoom: 8
-  });
+  map = new google.maps.Map(mapDiv, defaultMapOptions);
 }
-jQuery('document').ready(function(){
+
+function attachEvents() {
   jQuery('#getMapValues').click(function(){
     var valueString = '';
     valueString = valueString + '<p class="zoomLevel">Zoom level is set to: ' + map.getZoom() + '</p>';
@@ -17,23 +17,38 @@ jQuery('document').ready(function(){
     jQuery('#mapValues .modal').modal({backdrop: true, show: true})
   });
   jQuery('#jumpTo').click(function(){
-    jQuery('#mapJumpPoints .modal').modal({backdrop: true, show: true})
+    jQuery('#mapJumpPoints .modal').modal({backdrop: true, show: true});
   });
   jQuery('#mapJumpPoints .placebutton').click(function(){
     jQuery('.placebutton').removeClass('active').removeClass('btn-success').addClass('btn-info');
     jQuery(this).addClass('active').removeClass('btn-info').addClass('btn-success');
   });
   jQuery('#jumpMap').click(function(){
-    var statueOfLiberty = new google.maps.LatLng(40.6891,-74.0445);
-    var london = new google.maps.LatLng(51.5,-0.11);
-    var point = jQuery('.placebutton.active').attr('data-point');
-    point = parseInt(point);
-    if(point === 1) {
-      map.setCenter(london)
-    } else if (point ===2) {
-      map.setCenter(statueOfLiberty);
-    }
+    var selectedPoint = jQuery('.placebutton.active').attr('data-point');
+    selectedPoint = parseInt(selectedPoint);
+    var jumpPoint = new google.maps.LatLng(mapPoints[selectedPoint].Lat, mapPoints[selectedPoint].Lng);
+    map.setCenter(jumpPoint);
     jQuery('#closeJumpMap').trigger('click');
   });
+  jQuery('#setZoom').click(function(){
+    for(var i = 1; i < 25; i++) {
+      var option = '<option value="'+i+'">'+i+'</option>';
+      jQuery('#zoomLevelSelect .modal-body select').append(option);
+    }
+    jQuery('#zoomLevelSelect .modal').modal({backdrop: true, show: true});
+  });
+  jQuery('#zoomMap').click(function(){
+    var selectedZoom = jQuery('#zoomLevelSelect .modal-body select').find(":selected").text();
+    selectedZoom = parseInt(selectedZoom);
+    map.setZoom(selectedZoom);
+    jQuery('#closeZoomMap').trigger('click');
+  });
+  jQuery('#resetMap').click(function(){
+    map.setOptions(defaultMapOptions);
+  });
+}
+
+jQuery('document').ready(function(){
+  attachEvents();
 });
 google.maps.event.addDomListener(window, 'load', initMap);
