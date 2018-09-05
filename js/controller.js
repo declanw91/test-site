d8eb229f342ae05e51648f190253b01ae42f43aa
declanw91 <dwright1991@gmail.com>
2018-09-05 20:27:46 +0100

    bootstrap 4 update
    added picture carousel and progress bar

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,12 @@
 app.controller('myBlogController',function($scope, $http) {
     $http.get("GetBlogPosts.php")
     .then(function (response) {
+      jQuery('#blogLoadingBar .progress-bar').css('width','75%');
       var jsonstring = JSON.stringify(response.data);
       jsonstring = jsonstring.replace(/\+0000\s2\d{2,}/g, "");
       var jsondata = JSON.parse(jsonstring);
       $scope.blogPosts = jsondata;
+      jQuery('#blogLoadingBar .progress-bar').css('width','100%');
+      jQuery('#blogLoadingBar').hide();
     });
 });
