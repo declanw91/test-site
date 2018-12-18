94da22fbcc8bc823ff8d3b160e89c8c2ea772de6
declanw91 <dwright1991@gmail.com>
2018-12-18 20:56:20 +0000

    Added post date to twitter app

--- TwitterApp/js/controller.js
+++ TwitterApp/js/controller.js
@@ -4,6 +4,9 @@ app.controller('myTweetController', function($scope, $http) {
         var jsonstring = JSON.stringify(response.data);
         jsonstring = jsonstring.replace(/\+0000\s2\d{2,}/g, "");
         var jsondata = JSON.parse(jsonstring);
+        for(var i = 0; i < jsondata.length; i++){
+            jsondata[i].created_at = jsondata[i].created_at.split("+")[0];
+        }
         $scope.tweets = jsondata;
     });
 });
