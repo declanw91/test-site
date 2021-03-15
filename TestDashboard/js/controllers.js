b555c3d347eef6affc326dda6c95b423ad26f5ee
Declan <dwright1991@gmail.com>
2021-03-15 20:26:18 +0000

    Revert "Added test dashboardcode from old project"
    This reverts commit b9103d134ce0b822e738594755e3b9a354e80566.

--- TestDashboard/js/controllers.js
+++
@@ -1,9 +0,0 @@
-tabsapp.controller("tabsCtrl", function($scope) {
-  //intialise a few variabels based on example image values
-    $scope.activetab = "design";
-    $scope.stylecode = "2349";
-    $scope.description = "Description";
-    $scope.switchtab = function(name) {
-      $scope.activetab = name;
-    }
-});
